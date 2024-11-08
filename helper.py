7d586680a31cf2d8d070e04db07152d027be2664
Lars Hietbrink <larshietbrink@gmail.com>
2024-11-08 09:19:30 +0100

    Adding helper.py

---
+++ helper.py
@@ -0,0 +1,38 @@
+# Let's create a function to plot our loss curves...
+import matplotlib.pyplot as plt
+
+# Plot the validation and training curves:
+def plot_loss_curves(history):
+  """
+  Returns separate loss curves for training and validation metrics.
+
+  Args:
+    history: TensorFlow model History object (see: https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/History)
+  
+  Returns:
+    Two plots:
+      1. Training loss vs. Validation loss
+      2. Training accuracy vs. Validation accuracy
+  """
+  loss = history.history["loss"]
+  val_loss = history.history["val_loss"]
+
+  accuracy = history.history["accuracy"]
+  val_accuracy = history
+
+  epochs = range(len(history.history["loss"]))
+
+  # Plot loss
+  plt.plot(epochs, loss, label="training_loss")
+  plt.plot(epochs, val_loss, label="val_loss")
+  plt.title("Loss")
+  plt.xlabel("Epochs")
+  plt.legend()
+
+  # Plot accuracy
+  plt.figure()
+  plt.plot(epochs, accuracy, label="training_accuracy")
+  plt.plot(epochs, val_accuracy, label="val_accuracy")
+  plt.title("Accuracy")
+  plt.xlabel("Epochs")
+  plt.legend();
